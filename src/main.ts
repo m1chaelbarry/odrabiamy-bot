c58f786f5dd1a8c196db5391b089e0fe706f613a
michaelbarry <mbaryczka@protonmail.com>
2022-03-08 20:43:42 +0000

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -38,15 +38,14 @@ async function odrabiamyCommand(message: Message) {
     }
     await message.channel.send('https://emoji.gg/assets/emoji/loading.gif')
     const emoji = message.channel.lastMessage
-    await message.delete() 
     const response = await getResponse(exerciseDetails);
     const book_name = response.data.data[0].book.name
     const author = message.author.tag
     console.log(`${author} requested ${message.content} at ${getCurrentTime()}`)
-
+    
     if (message.content.includes('!str')) {
-
-
+        
+        
         for (let num = 0; num < response.data.data.length; num++) {
             let solution = response.data.data[num].solution;
             solution = encodeURI(solution);
@@ -56,113 +55,104 @@ async function odrabiamyCommand(message: Message) {
             const solutionScreenshot = await renderScreenshot(solution, excercise_number, page_number, book_name)
             await markAsVisited(response.data.data[num].id, config.odrabiamyAuth);
             if (!solutionScreenshot) break;
-        
+            
             await message.channel.send({
                 files: [solutionScreenshot],
             })
         }
-
+        
     } else if (message.content.includes('!split')) {
-
+        
         const response = await getResponse(exerciseDetails);
-
+        
         let solution = exerciseDetails.exerciseID
         ? response.data.data.filter((sol: apiSolution) => sol.id.toString() === exerciseDetails.exerciseID)[0].solution
         : response.data.data[0].solution;
         solution = encodeURI(solution);
         solution = decodeURI(solution)
-
+        
         const excercise_number = exerciseDetails.exerciseID 
-            ? response.data.data.filter((sol: apiSolution) => sol.id.toString() === exerciseDetails.exerciseID)[0].number
-            : response.data.data[0].number;
-
+        ? response.data.data.filter((sol: apiSolution) => sol.id.toString() === exerciseDetails.exerciseID)[0].number
+        : response.data.data[0].number;
+        
         const page_number = exerciseDetails.page
-
+        
         const subsection = solution.split('<hr>')
-
+        
         for (const element of subsection){
             const solutionScreenshot = await renderScreenshot(element, excercise_number, page_number, book_name)
             await markAsVisited(exerciseDetails.exerciseID ? exerciseDetails.exerciseID : response.data.data[0].id, config.odrabiamyAuth);
             if (!solutionScreenshot) return
-    
+            
             await message.channel.send({
                 files: [solutionScreenshot],
             })
         }
-
+        
     } else {
-
+        
         const response = await getResponse(exerciseDetails);
-
+        
         let solution = exerciseDetails.exerciseID
-            ? response.data.data.filter((sol: apiSolution) => sol.id.toString() === exerciseDetails.exerciseID)[0].solution
-            : response.data.data[0].solution;
-
+        ? response.data.data.filter((sol: apiSolution) => sol.id.toString() === exerciseDetails.exerciseID)[0].solution
+        : response.data.data[0].solution;
+        
         const excercise_number = exerciseDetails.exerciseID 
-            ? response.data.data.filter((sol: apiSolution) => sol.id.toString() === exerciseDetails.exerciseID)[0].number
-            : response.data.data[0].number;
-
+        ? response.data.data.filter((sol: apiSolution) => sol.id.toString() === exerciseDetails.exerciseID)[0].number
+        : response.data.data[0].number;
+        
         const page_number = exerciseDetails.page
-
+        
         const solutionScreenshot = await renderScreenshot(solution, excercise_number, page_number, book_name)
         await markAsVisited(exerciseDetails.exerciseID ? exerciseDetails.exerciseID : response.data.data[0].id, config.odrabiamyAuth);
         if (!solutionScreenshot) return
-
+        
         await message.channel.send({
             files: [solutionScreenshot],
         })
     }
-
-    // } else {
-        
-    //     const solutionScreenshot: Buffer | null = await odrabiamy(exerciseDetails, config.odrabiamyAuth);
-        
-    //         if (!solutionScreenshot) return 
-        
-    //         await message.channel.send({
-    //             files: [solutionScreenshot],
-    //         })
-    // }
+    
+    await message.delete() 
     if (emoji) {emoji.delete()}
-
-}
-//things for odrabiamyCommand
-async function getResponse(exerciseDetails: ExerciseDetails) {
-    return await axios.request({
-        method: 'GET',
-        url: `https://odrabiamy.pl/api/v2/exercises/page/premium/${exerciseDetails.page}/${exerciseDetails.bookID}`,
-        headers: {
-            'user-agent': 'new_user_agent-huawei-142',
-            Authorization: `Bearer ${config.odrabiamyAuth}`
+            
+    }
+        //things for odrabiamyCommand
+        async function getResponse(exerciseDetails: ExerciseDetails) {
+            return await axios.request({
+                method: 'GET',
+                url: `https://odrabiamy.pl/api/v2/exercises/page/premium/${exerciseDetails.page}/${exerciseDetails.bookID}`,
+                headers: {
+                    'user-agent': 'new_user_agent-huawei-142',
+                    Authorization: `Bearer ${config.odrabiamyAuth}`
+                }
+            });
         }
-    });
-}
-
-//things for odrabiamyCommand
-async function markAsVisited(exerciseID: string, authorization: string) {
-    axios.request({
-        method: 'POST',
-        url: `https://odrabiamy.pl/api/v2/exercises/${exerciseID}/visited`,
-        headers: {
-            'user-agent': 'new_user_agent-huawei-142',
-            Authorization: `Bearer ${authorization}`,
-        }
-    })
-}
-
-function getCurrentTime() {
-    var date_ob = new Date();
-    var day = ("0" + date_ob.getDate()).slice(-2);
-    var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    var year = date_ob.getFullYear();
-    
-    var date = year + "-" + month + "-" + day;
         
-    var hours = date_ob.getHours() + 1;
-    var minutes = date_ob.getMinutes();
-    var seconds = date_ob.getSeconds();
-    
-    var dateTime = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
+        //things for odrabiamyCommand
+        async function markAsVisited(exerciseID: string, authorization: string) {
+            axios.request({
+                method: 'POST',
+                url: `https://odrabiamy.pl/api/v2/exercises/${exerciseID}/visited`,
+                headers: {
+                    'user-agent': 'new_user_agent-huawei-142',
+                    Authorization: `Bearer ${authorization}`,
+                }
+            })
+        }
+        
+        function getCurrentTime() {
+            var date_ob = new Date();
+            var day = ("0" + date_ob.getDate()).slice(-2);
+            var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+            var year = date_ob.getFullYear();
+            
+            var date = year + "-" + month + "-" + day;
+            
+            var hours = date_ob.getHours() + 1;
+            var minutes = date_ob.getMinutes();
+            var seconds = date_ob.getSeconds();
+            
+            var dateTime = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
     return dateTime
 } 
 
