4b49b985ae16b80f0a54b1c938019441e6e0af96
m1chaelbarry <>
2023-10-20 16:01:45 +0000

    fixed rendering of katex

--- src/renderScreeshot.ts
+++ src/renderScreeshot.ts
@@ -1,27 +1,37 @@
-import nodeHtmlToImage from 'node-html-to-image'
+import puppeteer from 'puppeteer'
 
 export default function getExerciseImage(solution: string, excercise_number: string, page_number: string, book_name: string): Promise<Buffer | null> {
     return new Promise(async (resolve) => {
+
+        
+        const browser = await puppeteer.launch({timeout: 100000, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        const page = await browser.newPage();
+        await page.setViewport({width: 780, height: 1});
         let decoded_solution = solution
-        decoded_solution = `<style>html * {font-size: 14px;${decoded_solution.includes('class="math') ? '' : 'background: #36393E; color: #FFFFFF'};} .katex { font-size: 1.1em; }</style>` + decoded_solution
         decoded_solution = `<h1 style="font-size:30;"> ${excercise_number}/${page_number} </h1>` + decoded_solution
-	    decoded_solution = importKatex + decoded_solution
+        decoded_solution = `<style>html * {${decoded_solution.includes('<math-expr') ? '' : 'background: #36393E; color: #FFFFFF; font-family: MulishVariable,sans-serif;'}</style>` + decoded_solution
         decoded_solution = decoded_solution.replaceAll(/<object class="math small".*?>/g, '')
         decoded_solution = decoded_solution + `<p style="text-align: right; font-size: xx-small; color: #7F7F7F;"> ${book_name} &emsp;</p>`
 
-        const screenshot = await nodeHtmlToImage({
-            waitUntil: 'networkidle2',
-            html: decoded_solution
-        })
-        
-        console.log(decoded_solution);
-        
+        decoded_solution = importKatex + decoded_solution
+
+        const loaded = page.waitForNavigation({waitUntil: 'load'});
+        const loaded2 = page.waitForTimeout(2500)
+        await page.setContent(decoded_solution, {waitUntil: 'networkidle0'});
+        await loaded2;
+        await loaded
+        const bodyHeight = await page.evaluate(() => document.body.scrollHeight);
+        const bodyWidth = await page.evaluate(() => document.body.scrollWidth);
+        await page.setViewport({width: bodyWidth, height: bodyHeight});
+        const screenshot = await page.screenshot({fullPage: true})
 
         if (Buffer.isBuffer(screenshot)) {
             resolve(screenshot)
         } else {
             resolve(null)
         }
+
+        await browser.close();
 
 
     });
